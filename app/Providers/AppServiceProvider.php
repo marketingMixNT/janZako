4f1ad6ff9cb716e60decdaaa627f6d9b97c06a11
marekgacek45 <marekgacek45@gmail.com>
2024-10-04 07:46:25 +0200

    create cta observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,25 +2,27 @@
 
 namespace App\Providers;
 
+use App\Models\Cta;
 use App\Models\Home;
 use App\Models\Room;
 use App\Models\Slide;
 use App\Models\Gallery;
 use App\Models\Apartment;
-use App\Models\HomeApartmentsPage;
+use App\Observers\CtaObserver;
 use App\Models\HomeContactPage;
-use App\Models\HomeLocationPage;
 use App\Models\LocalAttraction;
 use App\Observers\HomeObserver;
 use App\Observers\RoomObserver;
+use App\Models\HomeLocationPage;
 use App\Observers\SlideObserver;
+use App\Models\HomeApartmentsPage;
 use App\Observers\GalleryObserver;
 use App\Observers\ApartmentObserver;
-use App\Observers\HomeApartmentsPageObserver;
 use Illuminate\Support\ServiceProvider;
 use App\Observers\HomeContactPageObserver;
-use App\Observers\HomeLocationPageObserver;
 use App\Observers\LocalAttractionObserver;
+use App\Observers\HomeLocationPageObserver;
+use App\Observers\HomeApartmentsPageObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -38,6 +40,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Apartment::observe(ApartmentObserver::class);
+        Cta::observe(CtaObserver::class);
         Gallery::observe(GalleryObserver::class);
         LocalAttraction::observe(LocalAttractionObserver::class);
         Room::observe(RoomObserver::class);
